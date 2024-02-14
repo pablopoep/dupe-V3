5f88ea83ca25f9de2945c4e00302017766c48d8c
pablopoep <123007142+pablopoep@users.noreply.github.com>
2024-02-14 15:39:41 +0100

    Add files via upload

---
+++ hide ui.lua
@@ -0,0 +1,19 @@
+local gemsleft = game:GetService('Players').LocalPlayer.PlayerGui.MainLeft.Left.Currency.Diamonds.Diamonds.Amount.Text
+local gemsleftpath = game:GetService('Players').LocalPlayer.PlayerGui.MainLeft.Left.Currency.Diamonds.Diamonds.Amount
+gemsleftpath:GetPropertyChangedSignal("Text"):Connect(function()
+    gemsleftpath.Text = gemsleft
+end)
+
+local gemsleaderstat = game:GetService('Players').LocalPlayer.leaderstats["💎 Diamonds"].Value
+local gemsleaderstatpath = game:GetService('Players').LocalPlayer.leaderstats["💎 Diamonds"]
+gemsleaderstatpath:GetPropertyChangedSignal("Value"):Connect(function()
+    gemsleaderstatpath.Value = gemsleaderstat
+end)
+
+local loading = game:GetService('Players').LocalPlayer.PlayerScripts.Scripts.Core["Process Pending GUI"]
+local noti = game:GetService('Players').LocalPlayer.PlayerGui.Notifications
+loading.Disabled = true
+noti:GetPropertyChangedSignal("Enabled"):Connect(function()
+    noti.Enabled = false
+end)
+noti.Enabled = false
